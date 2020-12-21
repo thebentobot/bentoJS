28aa759bd6df6f877cd844b276f549488e8d641f
Christian Krogh Nielsen <ChristianKN@christiansmbp2.home>
2020-12-21 01:58:49 +0100

    drained hotfix

--- commands/fun features/drained.js
+++ commands/fun features/drained.js
@@ -4,8 +4,7 @@ module.exports = {
     description: 'The drain command, only for drainers',
     usage: `drained`,
     run: async (client, message, args) => {
-        const user = '232584569289703424' || '123182368184991747' || '200435333198774273' || '145710786206105610' || '94540223051993088' || '321535048002306049' || '229341113503318018' || '192789985353531392'
-        if(message.author.id === user)  {
+        if(message.author.id === '232584569289703424' || message.author.id === '123182368184991747' || message.author.id === '200435333198774273' || message.author.id === '145710786206105610' || message.author.id === '94540223051993088' || message.author.id === '321535048002306049' || message.author.id === '229341113503318018' || message.author.id === '192789985353531392') {
             return message.channel.send('https://www.youtube.com/watch?v=vcAp4nmTZCA')
         } else {
             return message.channel.send('***YOU ARE NOT DRAINED***')
