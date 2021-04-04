784d38669b562d4d899455c3158b722be62fb5dc
christiankn <christiankroghnielsen@gmail.com>
2021-04-04 04:43:53 +0200

    instagram embedding

--- events/message.js
+++ events/message.js
@@ -8,7 +8,7 @@ const TikTokScraper = require('tiktok-scraper');
 const fetch = require('node-fetch');
 require('dotenv').config();
 const moment = require('moment');
-const userInstagram = require("user-instagram");
+const InstagramScraper = require('instatouch');
 const trim = (str, max) => (str.length > max ? `${str.slice(0, max - 3)}...` : str);
 
 var markdownEscape = function(text) {
@@ -193,8 +193,8 @@ module.exports = async (client, message) => {
               let testString = message.content
               //console.log(message.content)
               const query = testString.match(/\bhttps?:\/\/\S+/gi);
-              const finalQuery = query[0]
-              console.log(finalQuery)
+              //const finalQuery = query[0]
+              //console.log(finalQuery)
               const videoMeta = await TikTokScraper.getVideoMeta(query)
               const video = videoMeta.collector[0];
               //console.log(video)
@@ -218,53 +218,194 @@ module.exports = async (client, message) => {
                 return
               }
             }
-    /*
-    if (message.content.includes('instagram.com')) {
-      if (settings.instagram == 'disable') {
-        return
-      }
-      let checkUrl = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/
-      if (checkUrl.test(message) == false) {
-        return
-      }
-      let testString = message.content
-      let instaDataId = testString.split('/')[4]
-      console.log(instaDataId)
-      const data = await userInstagram.getPostData(instaDataId)
-      console.log(data)
-      let place = await data.location ? `, ${await data.location.name}` : ''
-      let verify = await data.owner.isVerified ? '✅' : ''
-      console.log(verify)
-      if (!data.childrenPictures.length && data.isVideo == false) {
-        try {
-          const embed = new Discord.MessageEmbed()
-          .setTitle(`${markdownEscape(await data.owner.full_name)}`)
-          .setDescription(await data.caption)
-          .setFooter(`${moment.unix(await data.takenAt).format("h:mm A dddd MMMM Do YYYY")}${place}`)
-          .setColor('#FD1D1D')
-          .setAuthor(`${await data.owner.username} ${verify}`, await data.owner.profilePicture, `https://www.instagram.com/${await data.owner.username}/`)
-          .setImage(await data.displayUrl)
-          await message.channel.send(embed)
-        } catch {
-          return
-        }
-      }
-      if (data.isVideo == false) {
-        try {
-          const embed = new Discord.MessageEmbed()
-          .setTitle(`${markdownEscape(data.owner.full_name)}`)
-          .setDescription(trim(data.caption, 2048))
-          .setFooter(`${moment.unix(data.takenAt).format("h:mm A dddd MMMM Do YYYY")}${place}`)
-          .setColor('#FD1D1D')
-          .setAuthor(data.owner.username, data.owner.profilePicture, `https://www.instagram.com/${data.owner.username}/`)
-          .setImage(data.childrenPictures[0].displayUrl)
-          await message.channel.send(embed)
-        } catch {
-          return
-        }
-      }
-    }
-    */
+            if (message.content.includes('instagram.com')) {
+              if (settings.instagram == 'disable') {
+                return
+              }
+              let checkUrl = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/
+              if (checkUrl.test(message) == false) {
+                return
+              }
+              let testString = message.content
+              const query = testString.match(/\bhttps?:\/\/\S+/gi);
+              const finalQuery = query[0]
+              const data = await InstagramScraper.getPostMeta(finalQuery, 
+                {
+                  session:`sessionid=${process.env.IGsessionID}`
+                })
+              const openData = data.graphql.shortcode_media
+              let place = await openData.location ? `, ${await openData.location.name}` : ''
+              let verify = await openData.owner.is_verified ? '✅' : ''
+        
+                // 1 pic, no vid
+              if (typeof openData.edge_sidecar_to_children === 'undefined' && openData.is_video == false) {
+              try {
+                  const embed = new Discord.MessageEmbed()
+                  .setTitle(`${markdownEscape(await openData.owner.full_name)}`)
+                  .setDescription(trim(markdownEscape(await openData.edge_media_to_caption.edges[0].node.text), 2048))
+                  .setFooter(`${moment.unix(await openData.taken_at_timestamp).format("h:mm A dddd MMMM Do YYYY")}${place}`)
+                  .setColor(process.env.COLOR)
+                  .setAuthor(`${await openData.owner.username} ${verify}`, await openData.owner.profile_pic_url, `https://www.instagram.com/${await openData.owner.username}/`)
+                  .setImage(await openData.display_url)
+                  await message.channel.send(embed)
+                } catch {
+                  return
+                }
+              }
+              // 1 vid, nothing else
+              if (openData.is_video == true && typeof openData.edge_sidecar_to_children === 'undefined') {
+                const response = await fetch(openData.video_url, {
+                  method: 'GET'
+                })
+                const buffer = await response.buffer()
+                const embed = new Discord.MessageEmbed()
+                  .setTitle(`${markdownEscape(await openData.owner.full_name)}`)
+                  .setDescription(trim(markdownEscape(await openData.edge_media_to_caption.edges[0].node.text), 2048))
+                  .setFooter(`${moment.unix(await openData.taken_at_timestamp).format("h:mm A dddd MMMM Do YYYY")}${place}`)
+                  .setColor(process.env.COLOR)
+                  .setAuthor(`${await openData.owner.username} ${verify}`, await openData.owner.profile_pic_url, `https://www.instagram.com/${await openData.owner.username}/`)
+                  await message.channel.send(embed)
+                  await message.channel.send(new Discord.MessageAttachment(buffer, 'video.mp4'))
+              }
+              // more pics than 1, videos featured as well
+              if (openData.edge_sidecar_to_children) {
+                function media (post) {
+                  if (post.is_video == false) {
+                      return post.display_url
+                  }
+                  if (post.is_video == false) {
+                    return ''
+                  }
+                }
+                
+                // borrowed function from the leaderboard command
+                function generateLBembed(lb) {
+                  const embeds = [];
+                  let k = 10;
+                  // loops through the children-posts for a post till there isn't any posts left
+                  for(let i =0; i < lb.edge_sidecar_to_children.edges.length; i += 1) {
+                      // loops through every children-post, one children-post = current: object Object
+                      const current = lb.edge_sidecar_to_children.edges[i]
+                      k += 1;
+                      // if children-post is not a video, it pushes a normal embed page with an image
+                      if (current.node.is_video == false) {
+                      const embed = new Discord.MessageEmbed()
+                      .setDescription(trim(markdownEscape(lb.edge_media_to_caption.edges[0].node.text), 2048))
+                      .setColor(process.env.COLOR)
+                      .setImage(media(current.node))
+                      .setTitle(`${markdownEscape(lb.owner.full_name)}`)
+                      .setFooter(`${moment.unix(lb.taken_at_timestamp).format("h:mm A dddd MMMM Do YYYY")}${place}`)
+                      .setAuthor(`${lb.owner.username} ${verify}`, lb.owner.profile_pic_url, `https://www.instagram.com/${lb.owner.username}/`)
+                      embeds.push(embed)
+                    }
+                      // if children-post has a video, it doesn't send an .setImage
+                      // it only sends the embed, and the video url
+                      // perhaps it's possible to check out of the function scope below, if an object has two objects (embed, current.node.video_url), and if so, then it uses that url
+                      if (current.node.is_video == true) {
+                        const embed = new Discord.MessageEmbed()
+                        .setDescription(trim(markdownEscape(lb.edge_media_to_caption.edges[0].node.text), 2048))
+                        .setColor(process.env.COLOR)
+                        .setTitle(`${markdownEscape(lb.owner.full_name)}`)
+                        .setFooter(`${moment.unix(lb.taken_at_timestamp).format("h:mm A dddd MMMM Do YYYY")}${place}`)
+                        .setAuthor(`${lb.owner.username} ${verify}`, lb.owner.profile_pic_url, `https://www.instagram.com/${lb.owner.username}/`)
+                        embeds.push([embed, current.node.video_url])
+                      }
+                  }
+                  return embeds;
+              }
+                  let currentPage = 0;
+                  const embeds = generateLBembed(openData)
+                  let queueEmbed;
+                  // if the first object in the array is a photo
+                  if (typeof embeds[currentPage][1] == 'undefined') {
+                    queueEmbed = await message.channel.send(`Current Picture: ${currentPage+1}/${embeds.length}`, embeds[currentPage]);
+                  }
+                  // if the first object in the array is a video
+                  if (typeof embeds[currentPage][1] != 'undefined') {
+                    queueEmbed = await message.channel.send(`Current Video: ${currentPage+1}/${embeds.length}`, embeds[currentPage]);
+                    const response = await fetch(embeds[currentPage][1], {
+                      method: 'GET'
+                    })
+                    const buffer = await response.buffer()
+                    await message.channel.send(new Discord.MessageAttachment(buffer, 'video.mp4'))
+                  }
+                  await queueEmbed.react('⬅️');
+                  await queueEmbed.react('➡️');
+                  await queueEmbed.react('❌');
+                  queueEmbed.edit();
+                  const filter = (reaction, user) => ['⬅️', '➡️', '❌'].includes(reaction.emoji.name) && (message.author.id === user.id);
+                  const collector = queueEmbed.createReactionCollector(filter);
+        
+                    collector.on('collect', async (reaction, user) => {
+                        if (reaction.emoji.name === '➡️') {
+                            if (currentPage < embeds.length-1) {
+                              currentPage++; // change page
+                              // if the current page doesn't have a video
+                              if (typeof embeds[currentPage][1] == 'undefined') {
+                                reaction.users.remove(user);
+                                queueEmbed.edit(`Current Picture: ${currentPage+1}/${embeds.length}`, embeds[currentPage]);
+                                // if the last page has a video
+                                if (typeof embeds[currentPage-1][1] != 'undefined') {
+                                  await message.channel.messages.fetch({ limit: 1 }).then(messages => { 
+                                    message.channel.bulkDelete(messages)});
+                                }
+                              }
+                              // if the current page has a video
+                              if (typeof embeds[currentPage][1] != 'undefined') {
+                                // if the last page has a video
+                                if (typeof embeds[currentPage-1][1] != 'undefined') {
+                                  await message.channel.messages.fetch({ limit: 1 }).then(messages => { 
+                                    message.channel.bulkDelete(messages)});
+                                }
+                                reaction.users.remove(user);
+                                queueEmbed.edit(`Current Video: ${currentPage+1}/${embeds.length}`, embeds[currentPage]);
+                                const response = await fetch(embeds[currentPage][1], {
+                                  method: 'GET'
+                                })
+                                const buffer = await response.buffer()
+                                await message.channel.send(new Discord.MessageAttachment(buffer, 'video.mp4'))
+                              }
+                            } 
+                          } else if (reaction.emoji.name === '⬅️') {
+                            if (currentPage !== 0) {
+                              --currentPage; // change page
+                              // if the current page doesn't have a video
+                              if (typeof embeds[currentPage][1] == 'undefined') {
+                                reaction.users.remove(user);
+                                queueEmbed.edit(`Current Picture ${currentPage+1}/${embeds.length}`, embeds[currentPage])
+                                // if the last page has a video
+                                if (typeof embeds[currentPage+1][1] != 'undefined') {
+                                  await message.channel.messages.fetch({ limit: 1 }).then(messages => { 
+                                    message.channel.bulkDelete(messages)});
+                                }
+                              }
+                              // if the current page has a video
+                              if (typeof embeds[currentPage][1] != 'undefined') {
+                                // if the last page has a video
+                                if (typeof embeds[currentPage+1][1] != 'undefined') {
+                                  await message.channel.messages.fetch({ limit: 1 }).then(messages => { 
+                                    message.channel.bulkDelete(messages)});
+                                }
+                                reaction.users.remove(user);
+                                queueEmbed.edit(`Current Video ${currentPage+1}/${embeds.length}`, embeds[currentPage])
+                                const response = await fetch(embeds[currentPage][1], {
+                                  method: 'GET'
+                                })
+                                const buffer = await response.buffer()
+                                await message.channel.send(new Discord.MessageAttachment(buffer, 'video.mp4'))
+                              }
+                            }
+                          } else {
+                            collector.stop();
+                            await queueEmbed.delete();
+                            if (typeof embeds[currentPage][1] != 'undefined') {
+                              await message.channel.messages.fetch({ limit: 1 }).then(messages => { 
+                                message.channel.bulkDelete(messages)});
+                            }
+                          }
+                    })
+              }
+            }
 
     const prefix = settings.prefix;
 
