ef4d3d1a09312bf22727776f545034df00f7db48
banner4422 <poulfrankibsen@gmail.com>
2021-04-12 19:34:31 +0200

    experiment again

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ client.mongoose = require('./utils/mongoose');
 client.categories = fs.readdirSync('./commands/');
 
 // fun feature for the boys
-schedule.scheduleJob('10 29 19 * * *', function(){
+schedule.scheduleJob('10 36 19 * * *', function(){
     client.channels.cache.get('668454896051159070').send('https://cdn.discordapp.com/attachments/802924837462999040/830465878474358854/v09044e00000c1cpjtd5o3sjgfc2blr0.mp4')
-    let voiceChannel = '668458345857941504'
+    let voiceChannel = client.channels.get('668458345857941504')
     voiceChannel.join().then(connection => {
         const dispatcher = connection.playFile('./utils/godaften.mp3');
      dispatcher.on("end", end => {
